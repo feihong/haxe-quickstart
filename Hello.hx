064e07e0b0d9034bdc4cae3dbc9dc7e78ef227bd
Feihong Hsu <feihong.hsu@gmail.com>
2020-10-04 18:45:09 -0500

    Write standard hello world program

---
+++ Hello.hx
@@ -0,0 +1,19 @@
+
+class Hello {
+  // https://github.com/jasononeil/hxrandom/blob/master/src/Random.hx
+  static inline function randomInt(from:Int, to:Int):Int {
+    return from + Math.floor(((to - from + 1) * Math.random()));
+  }
+
+  static inline function randomHanzi(): String {
+    var n = randomInt(0x4e00, 0x9fff);
+    return String.fromCharCode(n);
+  }
+
+  static public function main():Void {
+    // trace("你好世界");
+    for (i in 0...8) {
+      Sys.println(randomHanzi());
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# haxe-quickstart
-Feihong's Haxe quickstart
+# Feihong's Haxe quickstart
+
+# Installation
+
+    brew install haxe
+
+## Common commands
+
+Run a program as a script:
+
+    haxe --main Hello --interp
+
+## Sources
+
+- https://haxe.org/manual/introduction-hello-world.html
